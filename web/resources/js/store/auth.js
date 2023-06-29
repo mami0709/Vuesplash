92bf655f241ad7f32fbf68436e724a08c4c378e6
mami0709 <mmmxxxzelda@yahoo.co.jp>
2023-06-29 17:07:42 +0900

    CSRF対策の実装、会員登録機能の実装

--- web/resources/js/store/auth.js
+++ web/resources/js/store/auth.js
@@ -1,15 +1,29 @@
-const state = {}
+const state = {
+    // ログイン済みユーザーを保持する user を追加
+    user: null,
+};
 
-const getters = {}
+const getters = {};
 
-const mutations = {}
+const mutations = {
+    // user ステートの値を更新する setUser
+    setUser(state, user) {
+        state.user = user;
+    },
+};
 
-const actions = {}
+const actions = {
+    // 会員登録 API を呼び出す register アクション
+    async register(context, data) {
+        const response = await axios.post("/api/register", data);
+        context.commit("setUser", response.data);
+    },
+};
 
 export default {
-  namespaced: true,
-  state,
-  getters,
-  mutations,
-  actions
-}
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions,
+};
